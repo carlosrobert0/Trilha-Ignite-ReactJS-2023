5d5eae06ffc207b2bfd5f63ae7dd35e72e639966
STEFANINI-DOM\crneto <crneto@stefanini.com>
2022-07-05 14:50:01 -0300

    Estrutura da aplicação

--- Fundamentos do ReactJS/01-fundamentos-react-js/src/App.jsx
+++ Fundamentos do ReactJS/01-fundamentos-react-js/src/App.jsx
@@ -1,18 +1,31 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import styles from './App.module.css'
+
+import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
 
   return (
     <>
-      <Post 
-        author="Carlos Roberto" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium illum magnam corporis architecto porro et nemo nostrum cupiditate perferendis non aspernatur vel iusto, ducimus laborum reiciendis laudantium ratione? Quidem, voluptas?"
-      />
+      <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Carlos Roberto" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium illum magnam corporis architecto porro et nemo nostrum cupiditate perferendis non aspernatur vel iusto, ducimus laborum reiciendis laudantium ratione? Quidem, voluptas?"
+          />
 
-      <Post 
-        author="Jonas Receba" 
-        content="Accusantium illum magnam corporis architecto porro et nemo"
-      />
+          <Post 
+            author="Jonas Receba" 
+            content="Accusantium illum magnam corporis architecto porro et nemo"
+          />
+        </main>
+      </div>
     </>
   )
 }
---
+++ Fundamentos do ReactJS/01-fundamentos-react-js/src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+        className={styles.cover}
+      />
+    
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/45858897?v=4" />
+
+        <strong>Carlos Roberto</strong>
+        <span>Fullstack Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
