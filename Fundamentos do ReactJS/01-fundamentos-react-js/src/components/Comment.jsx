9d6cbc5720eebaf223579bdcd495727baa3b176c
STEFANINI-DOM\crneto <crneto@stefanini.com>
2022-07-05 19:15:02 -0300

    Components de Post e Comentario

---
+++ Fundamentos do ReactJS/01-fundamentos-react-js/src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/carlosrobert0.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Carlos Roberto</strong>
+              <time
+                title="11 de Maio às 08:13h"
+                dateTime="2022-05-11 08:13:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom devon, parabens!!👋</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
