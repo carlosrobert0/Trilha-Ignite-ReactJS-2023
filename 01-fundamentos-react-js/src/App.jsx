ca2f4044314aaf2dbbd54650fce6949af36a8fe1
carlosrobert0 <carlosroberto15755@gmail.com>
2023-02-01 18:06:42 -0300

    iterando no jsx

--- 01-fundamentos-react-js/src/App.jsx
+++ 01-fundamentos-react-js/src/App.jsx
@@ -6,14 +6,57 @@ import styles from './App.module.css'
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/carlosrobert0.png',
+      name: 'Carlos Roberto',
+      role: 'Desenvolvedor Fullstack',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design / doctorcare'},
+    ],
+    publishedAt: new Date('2023-01-31 16:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/carlosrobert0.png',
+      name: 'Carlos ',
+      role: 'Desenvolvedor Frontend',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design / doctorcare'},
+    ],
+    publishedAt: new Date('2023-01-30 16:00:00')
+  },
+]
+
 export function App() {
 
   return (
     <>
       <Header />
-      
+
       <div className={styles.wrapper}>
         <Sidebar />
+        <main>
+          { posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
         <Post />
       </div>
     </>
