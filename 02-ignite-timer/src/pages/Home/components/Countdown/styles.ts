9d39839598b3192d9c1ca88fa96b4a76aea4b8b5
carlosrobert0 <carlosroberto15755@gmail.com>
2023-02-19 18:05:39 -0300

    applying responsiveness in countdown

--- 02-ignite-timer/src/pages/Home/components/Countdown/styles.ts
+++ 02-ignite-timer/src/pages/Home/components/Countdown/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components"
-  export const CountdownContainer = styled.div`
+
+export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
   font-size: 10rem;
   line-height: 8rem;
@@ -12,6 +13,66 @@ import styled from "styled-components"
     background: ${({ theme }) => theme['gray-700']};
     padding: 2rem 1rem;
     border-radius: 8px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 8rem;
+    line-height: 6rem;
+    color: ${({ theme }) => theme['gray-100']};
+
+    display: flex;
+    gap: 0.7rem;
+
+    span {
+      background: ${({ theme }) => theme['gray-700']};
+      padding: 2rem 1rem;
+      border-radius: 4px;
+    }
+  }
+
+  @media (max-width: 550px) {
+    font-size: 6rem;
+    line-height: 4rem;
+    color: ${({ theme }) => theme['gray-100']};
+
+    display: flex;
+    gap: 0.5rem;
+
+    span {
+      background: ${({ theme }) => theme['gray-700']};
+      padding: 2rem 1rem;
+      border-radius: 4px;
+    }
+  }
+
+  @media (max-width: 452px) {
+    font-size: 4rem;
+    line-height: 3rem;
+    color: ${({ theme }) => theme['gray-100']};
+
+    display: flex;
+    gap: 0.2rem;
+
+    span {
+      background: ${({ theme }) => theme['gray-700']};
+      padding: 2rem 1rem;
+      border-radius: 4px;
+    }
+  }
+
+  @media (max-width: 360px) {
+    font-size: 3rem;
+    line-height: 3rem;
+    color: ${({ theme }) => theme['gray-100']};
+
+    display: flex;
+    gap: 0.2rem;
+
+    span {
+      background: ${({ theme }) => theme['gray-700']};
+      padding: 2rem 1rem;
+      border-radius: 4px;
+    }
   }
 `
 
