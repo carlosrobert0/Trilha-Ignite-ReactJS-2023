a1764ba1c46fbccb803716286c8f7e8a7c45fc39
Carlos Roberto <carlos.reis.33@hotmail.com>
2023-03-07 18:05:08 -0300

    Performance - applying context selectors

--- 03-dt-money/src/components/NewTransactionModal/index.tsx
+++ 03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,8 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as Dialog from '@radix-ui/react-dialog'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
-import { useContext } from 'react'
 import { Controller, useForm } from 'react-hook-form'
+import { useContextSelector } from 'use-context-selector'
 import * as z from 'zod'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
 import {
@@ -10,7 +10,7 @@ import {
   Content,
   Overlay,
   TransactionType,
-  TransactionTypeButton,
+  TransactionTypeButton
 } from './styles'
 
 const newTransactionFormSchema = z.object({
@@ -23,7 +23,13 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
-  const { createTransaction } = useContext(TransactionsContext)
+  const createTransaction = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.createTransaction
+    },
+  )
+
   const {
     register,
     handleSubmit,
