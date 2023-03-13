a86dfac8d6cf37361462fce30b2a07acf5c7e286
Carlos Roberto <carlos.reis.33@hotmail.com>
2023-03-13 19:12:26 -0300

    Product & Checkout - navigation with Link next/link

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -7,6 +7,7 @@ import Image from "next/image"
 import { stripe } from "@/lib/stripe"
 import 'keen-slider/keen-slider.min.css'
 import { GetStaticProps } from "next"
+import Link from "next/link"
 import Stripe from "stripe"
 
 interface HomeProps {
@@ -30,14 +31,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link key={product.id} href={`/product/${product.id}`}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
