f48d837ba11c31b14e7b50bdedba4ba0fcb7d9e4
Carlos Roberto <carlos.reis.33@hotmail.com>
2023-03-14 23:11:33 -0300

    Product & Checkout - success structure

--- 04-ignite-shop/src/pages/success.tsx
+++ 04-ignite-shop/src/pages/success.tsx
@@ -1,5 +1,23 @@
+import { ImageContainer } from "@/styles/pages/product";
+import { SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul, <strong>Diego Fernandes</strong>, sua <strong>Camiseta Beyound the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Volta ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
