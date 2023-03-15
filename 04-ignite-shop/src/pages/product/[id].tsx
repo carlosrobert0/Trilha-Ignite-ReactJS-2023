b1ebd098433e29b81a0cd0d8adb5879a2d9487ce
Carlos Roberto <carlos.reis.33@hotmail.com>
2023-03-15 00:58:46 -0300

    Product & Checkout - working with SEO

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -2,6 +2,7 @@ import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
 import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -44,7 +45,12 @@ export default function Product({ product }: ProductProps) {
   }
 
   return (
-    <ProductContainer>
+    <>
+      <Head>
+        <title>{product.name} - Ignite Shop</title>
+      </Head>
+
+      <ProductContainer>
       <ImageContainer>
         <Image src={product.imageUrl} alt="" width={520} height={480} />
       </ImageContainer>
@@ -63,6 +69,7 @@ export default function Product({ product }: ProductProps) {
         </button>
       </ProductDetails>
     </ProductContainer>
+    </>
   )
 }
 
--- 04-ignite-shop/src/pages/success.tsx
+++ 04-ignite-shop/src/pages/success.tsx
@@ -2,6 +2,7 @@ import { stripe } from "@/lib/stripe";
 import { ImageContainer } from "@/styles/pages/product";
 import { SuccessContainer } from "@/styles/pages/success";
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 import Image from 'next/image';
 import Link from "next/link";
 import Stripe from "stripe";
@@ -16,7 +17,14 @@ interface SuccessProps {
 
 export default function Success({ customerName, product }: SuccessProps) {
   return (
-    <SuccessContainer>
+    <>
+      <Head>
+        <title>Compra efetuada - Ignite Shop</title>
+
+        <meta name="robots" content="noindex" />
+      </Head>
+
+      <SuccessContainer>
       <h1>Compra efetuada!</h1>
 
       <ImageContainer>
@@ -32,6 +40,7 @@ export default function Success({ customerName, product }: SuccessProps) {
         Volta ao catálogo
       </Link>
     </SuccessContainer>
+    </>
   )
 }
 
